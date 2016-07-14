782f17398391028a2af659dbb72cc9551cc7a491
wangwendong <wangwendong@WM-Mac-mini.local>
2016-07-14 10:38:13 +0800

    init category oc

---
+++ Category_OC/Category_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Category_OC
+//
+//  Created by wangwendong on 16/7/14.
+//  Copyright © 2016年 maginawin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Category_OC/Category_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Category_OC
+//
+//  Created by wangwendong on 16/7/14.
+//  Copyright © 2016年 maginawin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
